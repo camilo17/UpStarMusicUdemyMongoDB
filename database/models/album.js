bbdede3248d9b610a90aef955f5358c475aef491
camilo17 <cami172013@gmail.com>
2017-05-17 08:49:06 -0500

    Created album and artist schema.

--- database/models/album.js
+++ database/models/album.js
@@ -1,1 +1,19 @@
 // Todo: create Album Schema
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const AlbumSchema = new Schema({
+    title: String,
+    date: Date,
+    copiesSold: Number,
+    numberTrack: Number,
+    image: String,
+    revenue: Number
+});
+
+// const Album = mongoose.model('Album', AlbumSchema);
+
+
+module.exports = AlbumSchema;
+
