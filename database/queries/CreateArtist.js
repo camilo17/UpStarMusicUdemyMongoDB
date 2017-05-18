252a5da25276a5cd6f4ca260c1b059c389ba9b11
camilo17 <cami172013@gmail.com>
2017-05-18 08:20:14 -0500

    created the SearchArtist query.

--- database/queries/CreateArtist.js
+++ database/queries/CreateArtist.js
@@ -7,7 +7,5 @@ const Artist = require('../models/artist');
  */
 module.exports = (artistProps) => {
     const artist = new Artist(artistProps);
-
     return artist.save();
-
 };
--- database/queries/DeleteArtist.js
+++ database/queries/DeleteArtist.js
@@ -7,6 +7,4 @@ const Artist = require('../models/artist');
  */
 module.exports = (_id) => {
     return Artist.remove({ _id });
-
-
 };
